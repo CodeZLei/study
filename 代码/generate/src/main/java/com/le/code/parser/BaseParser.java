a008888cd3d9b65de97f746830a29e91acb1ff02
zhanglei29 <zhanglei29@le.com>
2019-01-08 19:06:38 +0800

    去掉代码

--- 代码/generate/src/main/java/com/le/code/parser/BaseParser.java
+++
@@ -1,9 +0,0 @@
-package com.le.code.parser;
-
-import java.util.List;
-
-import com.le.code.bean.Table;
-
-public abstract class BaseParser {
-	abstract public List<Table> parse() throws Exception;
-}
--- 代码/generate/src/main/java/com/le/code/utils/KcoderConstants.java
+++
@@ -1,5 +0,0 @@
-package com.le.code.utils;
-
-public class KcoderConstants {
-	
-}
